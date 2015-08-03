668be2e8c7afe9b0028e87831715b8aa6c9a64d0
王少东 <wsdscy@gmail.com>
2015-08-03 20:22:15 +0800

    reconstruct TMesh with OpenMesh library

--- 3d_compression/src/TMesh/TMesh.h
+++ 3d_compression/src/TMesh/TMesh.h
@@ -1,188 +1,82 @@
 #pragma once
 #include <string>
-#include <vector>
-#include <fstream>
-#include <sstream>>
 #include <iostream>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <OpenMesh/Core/IO/MeshIO.hh>
 
-// Data type definition
-typedef struct vec {
-	double x;
-	double y;
-	double z;
-};
-
-typedef struct tri {
-	int index1;
-	int index2;
-	int index3;
-};
+typedef OpenMesh::TriMesh_ArrayKernelT<> MyMesh;
 
 class TMesh {
 public:
   TMesh() {}
-  ~TMesh() {
-    delete[] neighbors;
-    delete[] degrees;
-  }
-  void ReadObj(std::string file_name); // Read from wavefront obj file
+  ~TMesh() {}
+  void ReadMesh(std::string file_name);
+	int N_Vertices();
+	int N_Triangles();
 	bool IsOpen();
-  
-  std::vector<vec> vertices;
-  std::vector<tri> triangles;
-	std::vector<int> boundary_indices;
-	int* degrees; // Index start from 1
-  typedef struct Node {
-    int index;
-    Node *next;
-	} *NeighborList;
-  NeighborList* neighbors; // With head node
 private:
-  void buildTopo();
-	void maintainCCW(int index_focus, int index_neighbor);
-	int *vertex_count;
+	MyMesh mesh;
 };
 
-// Read from wavefront obj file
-void TMesh::ReadObj(std::string file_name) {
-	std::ifstream ifs(file_name.c_str());
-	if (ifs.fail()) {
-		std::cout << "Open Failed" << std::endl;
-		exit(-1);
+
+// Read mesh from file, build several
+// topological traits and store them in the mesh
+void TMesh::ReadMesh(std::string file_name) {
+	// Request vertex normals and read them from file
+	// If they're not provided, then calculate them
+	mesh.request_vertex_normals();
+
+	if (!mesh.has_vertex_normals()) {
+		std::cerr << "Error requiring vertex normals!" << std::endl;
+		exit(1);
 	}
 
-	int line_no = 0;
-	std::string line, first;
-	while (getline(ifs, line)) {
-		++line_no;
-		std::istringstream iss(line);
-		iss >> first;
-		if (first == "v") { // Read vertex
-			std::string x_str, y_str, z_str;
-			iss >> x_str >> y_str >> z_str;
-			double x, y, z;
-			x = std::stod(x_str);
-			y = std::stod(y_str);
-			z = std::stod(z_str);
-			vec v = { x, y, z };
-			this->vertices.push_back(v);
-		}
-		else if (first == "f") { // Read face
-			int v_indices[3];
-			for (int i = 0; i < 3; ++i) {
-				std::string temp;
-				iss >> temp;
-				auto split = temp.find_first_of("/");
-				std::string str = temp.substr(0, split);
-				int v_index = std::stoi(str);
-				v_indices[i] = v_index;
-			}
-			tri triangle = { v_indices[0], v_indices[1], v_indices[2] };
-			this->triangles.push_back(triangle);
-		}
-		else {
-			// Blank
-		}
-		line.clear();
-		first.clear();
-	}
-	ifs.close();
-
-	// Maintain boundary information
-	vertex_count = new int[vertices.size() + 1];
-	for (int i = 0; i < vertices.size() + 1; ++i)
-		vertex_count[i] = 0;
-	for (auto it = triangles.begin(); it != triangles.end(); ++it) {
-		++vertex_count[it->index1];
-		++vertex_count[it->index2];
-		++vertex_count[it->index3];
-	}
-	for (int i = 0; i < vertices.size() + 1; ++i) {
-		// For a manifold mesh, if a vertex appears only once, 
-		// it's on the boundary
-		if (vertex_count[i] == 1) {
-			boundary_indices.push_back(i);
-		}
+	OpenMesh::IO::Options opt;
+	if (!OpenMesh::IO::read_mesh(mesh, file_name, opt)) {
+		std::cerr << "read error" << std::endl;
+		exit(1);
 	}
 
-	buildTopo();
-}
-
-// Traverse triangles to build topological information
-void TMesh::buildTopo() {
-	auto v_count = vertices.size();
-	neighbors = new NeighborList[v_count + 1];
-	for (int i = 0; i < v_count + 1; ++i) {
-		neighbors[i] = new Node();
-	}
-	degrees = new int[v_count + 1];
-	for (int i = 0; i < v_count + 1; ++i) {
-		degrees[i] = 0;
+	if (!opt.check(OpenMesh::IO::Options::VertexNormal)) {
+		// Use face normals to update vertex normals
+		mesh.request_face_normals();
+		mesh.update_normals();
+		mesh.release_face_normals();
 	}
 
-	for (auto it = triangles.begin(); it != triangles.end(); ++it) {
-		int index1 = (*it).index1, index2 = (*it).index2, index3 = (*it).index3;
-		NeighborList v1_neighbors = neighbors[index1];
-		NeighborList v2_neighbors = neighbors[index2];
-		NeighborList v3_neighbors = neighbors[index3];
+	// Triangulate the mesh if it's not triangles
+	if (!mesh.is_triangles())
+		mesh.triangulate();
 
-		maintainCCW(index1, index2);
-		maintainCCW(index1, index3);
-		maintainCCW(index2, index1);
-		maintainCCW(index2, index3);
-		maintainCCW(index3, index1);
-		maintainCCW(index3, index2);
+	// Calculate and store vertices's valances
+	OpenMesh::VPropHandleT<MyMesh::Scalar> valances;
+	mesh.add_property(valances);
+
+	for (MyMesh::VertexIter v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
+		MyMesh::Scalar valance = 0.0f;
+
+		for (MyMesh::VertexVertexIter vv_it = mesh.vv_iter(*v_it); vv_it; ++vv_it)
+			++valance;
+		mesh.property(valances, *v_it) = valance;
 	}
 }
 
-// Update neighborList and maintain neighbors in ccw order
-void TMesh::maintainCCW(int index_focus, int index_neighbor) {
-	NeighborList my_neighbors = neighbors[index_focus];
-	++degrees[index_focus];
-	if (!my_neighbors->next) {
-		Node* node = new Node();
-		node->index = index_neighbor;
-		my_neighbors->next = node;
-	}
-	else {
-		Node* current = my_neighbors->next;
-		Node* previous = my_neighbors;
-		bool neighbor_in = false; // Check if neighbor is already in the list
-		bool insert_done = false; // Mark insertion success
-		vec focus = vertices[index_focus - 1];
-		vec neighbor = vertices[index_neighbor - 1];
-		vec vec_neighbor = { neighbor.x - focus.x, neighbor.y - focus.y, 0 };
-		while (current) {
-			if (current->index == index_neighbor) {
-				neighbor_in = true;
-				--degrees[index_focus];
-				break;
-			}
-			vec cursor = vertices[current->index - 1];
-			vec vec_cursor = { cursor.x - focus.x, cursor.y - focus.y, 0 };
-			double cross = vec_cursor.x * vec_neighbor.y - vec_cursor.y * vec_neighbor.x;
-			if (cross < 0) { // Insert neighbor before temp
-				Node* node = new Node();
-				node->index = index_neighbor;
-				node->next = current;
-				previous->next = node;
-				insert_done = true;
-				break;
-			}
-			previous = current;
-			current = current->next;
-		}
-		// If neighbor isn't on the list but not in ccw order
-		// with respect to other vertices on the list, then
-		// insert neighbor to the end
-		if (!neighbor_in && !insert_done) {
-			Node* node = new Node();
-			node->index = index_neighbor;
-			previous->next = node;
-		}
-	}
+
+// Return the number of vertices
+int TMesh::N_Vertices() {
+	return mesh.n_vertices;
 }
 
+
+// Return the number of triangles
+int TMesh::N_Triangles() {
+	return mesh.n_faces;
+}
+
+
+// Return true when mesh is open,
+// namely, having boundaries
 bool TMesh::IsOpen() {
-	return boundary_indices.empty();
+	// TODO
+	return true;
 }
