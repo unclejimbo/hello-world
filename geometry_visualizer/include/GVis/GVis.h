72fbad59de9a31fcd96a02c212c82bb15a867c13
王少东 <wsdscy@gmail.com>
2015-08-25 15:06:03 +0800

    gvis init

---
+++ geometry_visualizer/include/GVis/GVis.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//======standard libraries========
+#include <string>
+
+//======OpenGL Related============
+#include <GLFW/glfw3.h>
+
+namespace gvis {
+
+} // namespace gvis
--- geometry_visualizer/src/GVis.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class GVis {
-public:
-private:
-};
